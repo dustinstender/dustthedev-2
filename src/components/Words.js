c54198c37216d951dd418daea22b1c600b1f07db
Dustin Stender <dustinpstender@gmail.com>
2022-05-21 11:22:28 +0200

    fixed links

--- src/components/Words.js
+++ src/components/Words.js
@@ -4,9 +4,19 @@ export function Words() {
 			<h1 className="h1">Hello, I'm Dustin.</h1>
 			<p style={{ paddingBottom: '50%', width: '400px' }}>
 				You can see what I've been up to on{' '}
-				<a href="https://github.com/dustinstender">GitHub. </a>
+				<a
+					target="_blank"
+					href="https://github.com/dustinstender"
+					rel="noreferrer"
+				>
+					GitHub.{' '}
+				</a>
 				You can find some skate videos on my{' '}
-				<a href="https://www.youtube.com/channel/UCBHIfq8H3aqcuuLDzkUaTNw">
+				<a
+					target="_blank"
+					href="https://www.youtube.com/channel/UCBHIfq8H3aqcuuLDzkUaTNw"
+					rel="noreferrer"
+				>
 					Youtube.
 				</a>
 			</p>
