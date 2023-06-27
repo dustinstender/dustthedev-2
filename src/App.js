71f56634f8e0e34a29aa0831c61b583e63e9832f
Dustin Stender <dustinpstender@gmail.com>
2023-06-27 21:01:14 +0200

    mobile responsive sort of

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import { Cartoon } from './components/Cartoon';
 import { Words } from './components/Words';
 
 function App() {
-	return (
-		<div className='home'>
-			<div>
+	const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+	useEffect(() => {
+		// Update the window width state when the window is resized
+		const handleResize = () => {
+			setWindowWidth(window.innerWidth);
+		};
+
+		window.addEventListener('resize', handleResize);
+
+		// Clean up the event listener when the component is unmounted
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
+
+	if (windowWidth < 800) {
+		return (
+			<div
+				style={{
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+					height: '100vh',
+				}}
+			>
 				<Words />
+			</div>
+		);
+	}
+
+	return (
+		<div className="home">
+			<div>
+				<Words isWideScreen={true} />
 			</div>
 			<div>
 				<Cartoon />
--- src/components/Words.js
+++ src/components/Words.js
@@ -1,42 +1,39 @@
-import { useState } from "react";
-import { getRecentlyPlayedTrackData } from "../api/getRecentlyPlayedTrackData";
-import { Spotify } from "react-spotify-embed";
+import { useState } from 'react';
+import { getRecentlyPlayedTrackData } from '../api/getRecentlyPlayedTrackData';
+import { Spotify } from 'react-spotify-embed';
 
-export function Words() {
-
-	const [songData, setSongData] = useState(undefined)
+export function Words({ isWideScreen }) {
+	const [songData, setSongData] = useState(undefined);
 
 	const retrieveSongData = async () => {
 		if (!songData) {
 			try {
 				const songData = await getRecentlyPlayedTrackData();
-				setSongData(songData)
-			
-			  } catch (error) {
+				setSongData(songData);
+			} catch (error) {
 				console.error('Error retrieving song data:', error);
-			  }
+			}
 		}
-	}
+	};
 
-	retrieveSongData()
+	retrieveSongData();
+
+	const wordsStyle = isWideScreen ? '50%' : '';
 
 	return (
-		<div className="words">
+		<div style={{ paddingRight: wordsStyle }}>
 			<h1 className="h1">Hello, I'm Dustin.</h1>
 			<p style={{ width: '400px' }}>
 				You can see what I've been coding on{' '}
-				<a
-					href="https://github.com/dustinstender"
-				>
-					GitHub
-				</a>.
-				You can find some skate videos on my{' '}
-				<a
-					href="https://www.youtube.com/channel/UCBHIfq8H3aqcuuLDzkUaTNw"
-				>
+				<a href="https://github.com/dustinstender">GitHub</a>. You can find some
+				skate videos on my{' '}
+				<a href="https://www.youtube.com/channel/UCBHIfq8H3aqcuuLDzkUaTNw">
 					Youtube
-				</a>.
-				<p style={{fontSize: 14, paddingTop: 5}} >What I'm currently listening to:</p>
+				</a>
+				.
+				<p style={{ fontSize: 14, paddingTop: 5 }}>
+					What I'm currently listening to:
+				</p>
 				{songData && <Spotify wide link={songData.external_urls.spotify} />}
 			</p>
 		</div>
